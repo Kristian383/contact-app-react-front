c1ae8223625c868f091e18d595d80d5320b28099
Kristian <kristian383@gmail.com>
2022-05-28 19:26:56 +0200

    DeleteModal html structure

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import classes from "./DeleteModal.module.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function DeleteModal() {
+function DeleteModal({ contact }) {
+    function closeDeleteModal() { }
+    function sendDeleteEmail() { }
+
     return (
-        <div>DeleteModal</div>
+        <div className={`${classes["delete-modal-wrapper"]} ${classes.open}`} id="modal" ref="modal">
+            <div className={classes["delete-modal"]}>
+                <div className={classes["modal-header"]}>
+                    <span ><FontAwesomeIcon icon={["fas", "trash"]} /> </span>
+                    <h3>Delete contact - {{ contact }}?</h3>
+                </div>
+                <div className={classes["modal-body"]}>
+                    <p>
+                        You'll <b>permanently</b> lose all the data and there is no way back.
+                    </p>
+                </div>
+                <div className={classes["modal-footer"]}>
+                    <button className={`${classes.btn} classes["cancel-btn"]`} onClick={closeDeleteModal}>Cancel</button>
+                    <button className={`${classes.btn} classes["delete-btn"]`} onClick={sendDeleteEmail}>Delete</button>
+                </div>
+            </div >
+        </div >
     )
 }
 
