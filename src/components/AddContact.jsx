bf83b18bd41fdc7af80714523bcaa9b335918786
Kristian <kristian383@gmail.com>
2022-05-29 12:19:14 +0200

    two way data binding on form if edit mode

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,33 +1,104 @@
-import React, { useRef, useState } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import ButtonSave from './ui/ButtonSave'
 import classes from "./AddContact.module.scss"
+import { useParams, useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 function AddContact() {
 
-    const firstName = useRef("")
-    const lastName = useRef("")
-    const email = useRef("")
-    const phoneNumber = useRef("")
-    const [editMode, setEditMode] = useState(null)
+    const [firstName, setfirstName] = useState("")
+    const [lastName, setlastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setphoneNumber] = useState("")
+    const [editMode, setEditMode] = useState(false)
 
-    const firstNameIsValid = useRef("")
-    const lastNameIsValid = useRef("")
-    const emailIsValid = useRef("")
-    const phoneNumberIsValid = useRef("")
+    const [firstNameIsValid, setfirstNameIsValid] = useState(true)
+    const [lastNameIsValid, setlastNameIsValid] = useState(true)
+    const [emailIsValid, setemailIsValid] = useState(true)
+    const [phoneNumberIsValid, setphoneNumberIsValid] = useState(true)
+
+    const contacts = useSelector(state => state.contact.contacts)
+    const params = useParams();
+
+    useEffect(() => {
+        const emailParam = params.email;
+
+        if (!emailParam) {
+            return;
+        }
+
+        if (contacts.length) {
+            const contact = contacts.find(el => el.email === emailParam)
+            setfirstName(contact.first_name);
+            setlastName(contact.last_name);
+            setEmail(params.email);
+            setphoneNumber(contact.phone_number);
+            setEditMode(true)
+        }
+
+
+    }, [contacts, params.email])
+
+
+
+    function validateForm() {
+        setfirstNameIsValid(true)
+        setlastNameIsValid(true)
+        setemailIsValid(true)
+        setphoneNumberIsValid(true)
+
+        if (!firstName.length || firstName.length > 40) {
+            setfirstNameIsValid(false)
+        }
+        if (!lastName.length || lastName.length > 40) {
+            setlastNameIsValid(false)
+        }
+        if (!email.length || email.length > 80) {
+            setemailIsValid(false)
+        }
+        if (phoneNumber.length === 0 || isNaN(phoneNumber)) {
+            setphoneNumberIsValid(false)
+        }
+
+        if (
+            firstNameIsValid &&
+            lastNameIsValid &&
+            emailIsValid &&
+            phoneNumberIsValid
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const history = useHistory()
 
     function addContact(e) {
         e.preventDefault();
+        if (validateForm() === false) {
+            return;
+        }
 
+        const payload = {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            phone: phoneNumber,
+            editMode: editMode,
+        };
+        console.log(payload)
+        history.push("/contacts")
     }
 
     return (
         <div className={classes["add-contact-container"]}>
-            <form>
+            <form onSubmit={addContact} >
                 <div className={classes["form-title"]}>Add Contact</div>
 
                 <div className={classes["input-group"]}>
                     <label htmlFor="first">First Name</label>
-                    <input type="text" ref={firstName} id="first" />
+                    <input type="text" onChange={e => setfirstName(e.target.value)} value={firstName} id="first" />
                     {!firstNameIsValid && <p className={classes["error-text"]} >
                         Please insert less than 40 characters.
                     </p>}
@@ -35,7 +106,7 @@ function AddContact() {
 
                 <div className={classes["input-group"]}>
                     <label htmlFor="last">Last Name</label>
-                    <input type="text" id="last" ref={lastName} />
+                    <input type="text" id="last" onChange={e => setlastName(e.target.value)} value={lastName} />
                     {!lastNameIsValid && <p className={classes["error-text"]} >
                         Please insert less than 40 characters.
                     </p>}
@@ -46,7 +117,8 @@ function AddContact() {
                     <input
                         type="email"
                         id="email"
-                        ref={email}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                         disabled={editMode ? true : false}
                     />
                     {!emailIsValid && <p className={classes["error-text"]} >
@@ -55,14 +127,14 @@ function AddContact() {
                 </div>
                 <div className={classes["input-group"]}>
                     <label htmlFor="phone">Phone Number</label>
-                    <input type="tel" id="phone" ref={phoneNumber} />
+                    <input onChange={e => setphoneNumber(e.target.value)} type="tel" id="phone" value={phoneNumber} />
                     {!phoneNumberIsValid && <p className={classes["error-text"]} >
                         Please insert number with less than 20 digits.
                     </p>}
                 </div>
                 <div className={classes["save-btn-container"]}>
                     {/* <button-save @click.prevent="addContact"></button-save> */}
-                    <ButtonSave onClick={addContact} />
+                    <ButtonSave />
                 </div>
             </form >
         </div >
